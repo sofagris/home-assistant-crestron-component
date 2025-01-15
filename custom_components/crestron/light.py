68f2723ba7a9b53dbb0537f4b47b1f86aa582ad4
Georges Khalil <georgesak@gmail.com>
2025-01-14 19:37:56 -0500

    Updates to the light component: - Remove light type, it was not useful. We always default to a dimmable light. - Remove deprecated supported_features and replace with attr_color_mode and attr_supported_color_modes - Other minor fixes

--- custom_components/crestron/light.py
+++ custom_components/crestron/light.py
@@ -3,9 +3,9 @@ import voluptuous as vol
 import logging
 
 import homeassistant.helpers.config_validation as cv
-from homeassistant.components.light import LightEntity, SUPPORT_BRIGHTNESS
+from homeassistant.components.light import ATTR_BRIGHTNESS, ColorMode, LightEntity
 from homeassistant.util import slugify
-from homeassistant.const import CONF_NAME, CONF_TYPE
+from homeassistant.const import CONF_NAME
 from .const import HUB, DOMAIN, CONF_BRIGHTNESS_JOIN, CONF_BRIGHTNESS_DEFAULT
 
 _LOGGER = logging.getLogger(__name__)
@@ -13,7 +13,6 @@ _LOGGER = logging.getLogger(__name__)
 PLATFORM_SCHEMA = vol.Schema(
     {
         vol.Required(CONF_NAME): cv.string,
-        vol.Required(CONF_TYPE): cv.string,
         vol.Required(CONF_BRIGHTNESS_JOIN): cv.positive_int,    
         vol.Optional(CONF_BRIGHTNESS_DEFAULT, default=128): cv.positive_int,        
     },
@@ -32,8 +31,8 @@ class CrestronLight(LightEntity):
         self._name = config.get(CONF_NAME)
         self._brightness_join = config.get(CONF_BRIGHTNESS_JOIN)
         self._default_brightness = config.get(CONF_BRIGHTNESS_DEFAULT)
-        if config.get(CONF_TYPE) == "brightness":
-            self._supported_features = SUPPORT_BRIGHTNESS
+        self._attr_color_mode = ColorMode.BRIGHTNESS
+        self._attr_supported_color_modes = {ColorMode.BRIGHTNESS}
         self._unique_id = slugify(f"{DOMAIN}_light_{self._name}")
 
     async def async_added_to_hass(self):
@@ -54,33 +53,24 @@ class CrestronLight(LightEntity):
         return self._name
 
     @property
-    def supported_features(self):
-        return self._supported_features
-
-    @property
     def should_poll(self):
         return False
 
     @property
     def brightness(self):
-        if self._supported_features == SUPPORT_BRIGHTNESS:
-            return int(self._hub.get_analog(self._brightness_join) / 257)
+        return int(self._hub.get_analog(self._brightness_join) / 257)
 
     @property
     def is_on(self):
-        if self._supported_features == SUPPORT_BRIGHTNESS:
-            if int(self._hub.get_analog(self._brightness_join) / 257) > 0:
-                return True
-            else:
-                return False
+        return bool(self.brightness)
 
     @property
     def unique_id(self):
         return self._unique_id
 
     async def async_turn_on(self, **kwargs):
-        if "brightness" in kwargs:
-            self._hub.set_analog(self._brightness_join, int(kwargs["brightness"] * 257))
+        if ATTR_BRIGHTNESS in kwargs:
+            self._hub.set_analog(self._brightness_join, int(kwargs[ATTR_BRIGHTNESS] * 257))
         else:
             self._hub.set_analog(self._brightness_join, self._default_brightness * 257)
 
